290da9f6b58a38dd0ba91b1a0b05d00f8a86a04c
Slava Semeniuk <slava.semeniuk@ekreative.com>
2024-04-23 16:23:06 +0300

    Initial Commit

---
+++ MutableCompositionIssue/MutableCompositionIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MutableCompositionIssueApp.swift
+//  MutableCompositionIssue
+//
+//  Created by Semeniuk Slava on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MutableCompositionIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
